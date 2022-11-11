d788936d5ab57d46c6e47acbc380043ea5677cbd
heyitsuzair <uzairdevil354123@gmail.com>
2022-11-11 20:45:16 +0500

    Login Page Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SinglePost from "./pages/SinglePost";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="post/:id" element={<SinglePost />} />
+          <Route path="login" element={<Login />} />
         </Routes>
       </Router>
     </div>
