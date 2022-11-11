00180248044f018afd8ee7b610d7d9facf0646cf
heyitsuzair <uzairdevil354123@gmail.com>
2022-11-11 23:23:18 +0500

    Fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-<<<<<<< HEAD
-import { getPosts, wpposts } from "../utils/api";
-=======
 import { wpposts } from "../utils/api";
->>>>>>> dashboard
 import { Link } from "react-router-dom";
 import { Interweave } from "interweave";
 import Moment from "react-moment";
