88649b997f2309f7bb63359da90c54c7fce29a3e
heyitsuzair <uzairdevil354123@gmail.com>
2022-11-15 00:23:51 +0500

    Custom Endpoint Integerated

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { fetchBlogPosts } from "../utils/api";
+import Spinner from "./Spinner";
 
 export default function Posts({ pageNo }) {
-  console.log(pageNo);
-  return <div>Posts</div>;
+  // Converting String To Integer
+  const pageNoInt = parseInt(pageNo);
+
+  const [currentPage, setCurrentPage] = useState(pageNoInt);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const { data } = await axios.get(fetchBlogPosts + pageNoInt);
+      setPosts(data.posts_data);
+      setTotalPages(data.page_count);
+      setLoading(false);
+    } catch (error) {
+      console.warn(error.response.data.message);
+      setError(error.response.data.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      {loading === true ? (
+        <Spinner />
+      ) : (
+        <>{error && <div className="alert alert-danger">{error}</div>}</>
+      )}
+    </>
+  );
 }
